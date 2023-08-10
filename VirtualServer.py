18a73c3f5a89aa98846137efee22f3e69a2f8c33
ArsalanAli400 <arsalan.ali.safeer@gmail.com>
2023-08-10 23:52:24 +0500

    Added GPS-Tracer Modifitions

--- VirtualServer.py
+++ VirtualServer.py
@@ -1,30 +1,26 @@
-import requests
 import random
 from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template('index.html')
 
-while True:  
-# this route only handle the rendering of index.html
+@app.route("/GPGGS")
+def lat():
+    # process the data using Python code
+    name="GPS-tracer"
+    id = 00000
+    latitude = random.uniform(33.565111, 33.565200)
+    longitude = random.uniform(73.016500, 73.016913)
+    altitude = 0.00
+    velocity=0.00
+    gps_string = {"name": name, "id": id, "latitude": latitude, "longitude": longitude, "altitude": altitude,
+                  "velocity": velocity}
+    response = jsonify(gps_string)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response, 200
 
-    # @app.route("/index.html")
-    # def main():
-    #    return render_template('index.html')
-
-    @app.route("/lat")
-    def lat():
-        lat = random.uniform(33.565111, 33.565200)
-        templateData = {'data': lat}
-        return jsonify(templateData), 200
-
-    @app.route("/long")
-    def long():
-        long = random.uniform(73.016500, 73.016913)
-        templateData = {'data' : long}
-        return jsonify(templateData), 200
-# this route handling the request send to the /update uri
-    
-    if __name__ == "__main__":
-        app.run()
-
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9090, debug=True)
